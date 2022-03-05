d843327028a2e8728ce1b0de1ba61c46811d7701
Thales Maciel <thalesmsm12@gmail.com>
2022-03-05 17:15:05 -0300

    cria programa que retorna o maior de tres numeros

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/script3.js
@@ -0,0 +1,11 @@
+const num1 = 1;
+const num2 = 2;
+const num3 = 3;
+
+if (num1 > num2 && num1 > num3) {
+  console.log('o numero 1 é o maior')
+} else if (num2 > num1 && num2> num3) {
+  console.log('o numero 2 é o maior')
+} else if (num3 > num1 && num3> num2) {
+  console.log('o numero 3 é o maior')
+}
