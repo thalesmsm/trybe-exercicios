1225d59e7eb8048a90c556b6d446180c67fe9bf8
Thales Maciel <thalesmsm12@gmail.com>
2022-03-05 16:59:17 -0300

    cria progrma com operaçoes aritimeticas

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/script1.js
@@ -0,0 +1,8 @@
+const a = 20;
+const b = 12;
+
+console.log(`${a} + ${b} = ${a + b}`)
+console.log(`${a} - ${b} = ${a - b}`)
+console.log(`${a} * ${b} = ${a * b}`)
+console.log(`${a} / ${b} = ${a / b}`)
+console.log(`${a} ** ${b} = ${a ** b}`)
