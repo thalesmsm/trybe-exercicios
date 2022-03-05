7314a99a2bda65509f0f66ba48c4ff722d20f101
Thales Maciel <thalesmsm12@gmail.com>
2022-03-05 17:09:50 -0300

    cria progama de operadores condicionais

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/script2.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/script2.js
@@ -1,8 +1,8 @@
-const num1 = 23;
-const num2 = 65;
+const num1 = 20;
+const num2 = 34;
 
 if (num1 > num2) {
-  console.log(num1 + ' é o maior numero')
+  console.log('Número 1 é o maior')
 } else {
-  console.log(num2 + ' é o maior numero')
+  console.log('Número 2 é o maior')
 }
