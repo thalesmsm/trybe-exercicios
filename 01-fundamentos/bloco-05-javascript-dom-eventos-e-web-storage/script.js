3d2d19021ad6e336e5774c2fce038e333c051fc7
Thales Maciel <thalesmsm12@gmail.com>
2022-03-15 13:57:11 -0300

    corrige cores

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/script.js
@@ -1,13 +1,13 @@
-document.getElementById("header-container").style.backgroundColor = 'green';
-document.querySelectorAll(".emergency-tasks")[0].style.backgroundColor = 'salmon';
+document.getElementById("header-container").style.backgroundColor = '#00B069';
+document.querySelectorAll(".emergency-tasks")[0].style.backgroundColor = '#FF9F84';
 let emergencyTasks = document.querySelectorAll(".emergency-tasks h3");
 console.log(document.querySelectorAll(".emergency-tasks h3"));
 for (let i = 0; i < emergencyTasks.length; i += 1){
-  document.querySelectorAll(".emergency-tasks h3")[i].style.backgroundColor = 'purple'
+  document.querySelectorAll(".emergency-tasks h3")[i].style.backgroundColor = '#A54DF3'
 }
-document.querySelectorAll(".no-emergency-tasks")[0].style.backgroundColor = 'yellow'
+document.querySelectorAll(".no-emergency-tasks")[0].style.backgroundColor = '#F9DB5E'
 let noEmergencyTasks = document.querySelectorAll(".no-emergency-tasks h3");
 for (let i = 0; i < noEmergencyTasks.length; i += 1){
-  document.querySelectorAll(".no-emergency-tasks h3")[i].style.backgroundColor = 'black'
+  document.querySelectorAll(".no-emergency-tasks h3")[i].style.backgroundColor = '#232525'
 }
-document.getElementById("footer-container").style.backgroundColor = 'darkgreen'
+document.getElementById("footer-container").style.backgroundColor = '#003533'
