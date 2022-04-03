032f5fbfa2412229dcae58356f34d0a1666970ae
Thales Maciel <thalesmsm12@gmail.com>
2022-04-02 21:59:01 -0300

    enunciados

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/exercicios-pt1.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/exercicios-pt1.js
@@ -1,3 +1,7 @@
+// Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+// Modifique a estrutura da função para que ela seja uma arrow function .
+// Modifique as concatenações para template literals .
+
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -11,6 +15,10 @@ const testingScope = (escopo) => {
 }
 
 testingScope(true);
+
+// Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
 
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/exercicios-pt2.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/exercicios-pt2.js
@@ -1,3 +1,6 @@
+// Crie uma função que receba um número e retorne seu fatorial.
+// Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+
 const fatorial = (number) => {
   let res = 1;
 
@@ -10,6 +13,7 @@ const fatorial = (number) => {
 
 console.log(fatorial(5));
 
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
 
 const longestWord = (phrase) => {
   let arr = phrase.split(' ');
