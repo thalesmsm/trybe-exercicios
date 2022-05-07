36545337cfe652f6fbdb49959d41ef84715864c2
Thales Maciel <thalesmsm12@gmail.com>
2022-05-07 10:17:06 -0300

    iniciando no react

---
+++ 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-01-hello-world-com-react/header/src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    const {title} = this.props
+    return <h1 className="title">{title}</h1>
+  }
+}
+
+export default Header;
